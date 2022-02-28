a809b85f5f57b79505fdbc8e50b581b928bfd682
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-02-27 19:37:48 -0800

    Create delete support, and edit view with formInput partial that is used by new view too

--- super_team_picker/routes/cohorts.js
+++ super_team_picker/routes/cohorts.js
@@ -6,57 +6,74 @@ const router = express.Router();
 router.get("/", (req, res) => {
   knex("cohorts")
     .orderBy("created_at", "DESC")
-    .then(cohorts => {
-      res.render("cohorts/index", {cohorts: cohorts});
+    .then((cohorts) => {
+      res.render("cohorts/index", { cohorts: cohorts });
     });
 });
 
 router.get("/new", (req, res) => {
-  const members = req.body.members;
-  const logo_url = req.body.logo_url;
-  const name = req.body.name;
-
-  res.render("cohorts/new", {
-    name: name,
-    members: members,
-    logo_url: logo_url
-  });
+  res.render("cohorts/new", { cohort: false });
 });
 
 router.get("/:id", (req, res) => {
-  const id = req.params.id;
-
   knex("cohorts")
-    .where("id", id)
+    .where("id", req.params.id)
     .first()
-    .then(cohort => {
-      res.render("cohorts/show", {cohort: cohort});
+    .then((cohort) => {
+      res.render("cohorts/show", { cohort: cohort });
     });
 });
 
 router.post("/new", (req, res) => {
   const members = req.body.members;
-  const logo_url = req.body.logo_url;
   const name = req.body.name;
 
-  if (name.length > 0 || logo_url.length > 0 || members.length > 0) {
+  if (name.length > 0 || members.length > 0) {
     knex("cohorts")
       .insert({
         name: name,
         members: members,
-        logo_url: logo_url,
+        logo_url: req.body.logo_url,
       })
       .returning("*")
-      .then(cohort => {
-        res.redirect(`/:${cohort.id}`);
+      .then((cohorts) => {
+        const cohort = cohorts[0];
+        res.redirect(`/cohorts/${cohort.id}`);
       });
   } else {
-    res.render("cohorts/new", {
-      name: name,
-      members: members,
-      logo_url: logo_url
+    res.render("cohorts/new", { cohort: false });
+  }
+});
+
+router.get("/:id/edit", (req, res) => {
+  knex("cohorts")
+    .where("id", req.params.id)
+    .first()
+    .then((cohort) => {
+      res.render("cohorts/edit", { cohort: cohort });
     });
-  };
+});
+
+router.patch("/:id", (req, res) => {
+  knex("cohorts")
+    .where("id", req.params.id)
+    .update({
+      name: req.body.name,
+      logo_url: req.body.logo_url,
+      members: req.body.members,
+    })
+    .then(() => {
+      res.redirect(`/cohorts/${req.params.id}`);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  knex("cohorts")
+    .where("id", req.params.id)
+    .delete()
+    .then(() => {
+      res.redirect("/cohorts");
+    });
 });
 
 module.exports = router;
