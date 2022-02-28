8611ca798cf14aac0b27a82980958e75ba243614
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-02-27 21:12:59 -0800

    Create the assign teams partial and include it in show view

--- super_team_picker/routes/cohorts.js
+++ super_team_picker/routes/cohorts.js
@@ -15,12 +15,71 @@ router.get("/new", (req, res) => {
   res.render("cohorts/new", { cohort: false });
 });
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function getRandomizedArray(membersArray) {
+  const randomizedArray = [];
+  for (let i = membersArray.length-1; i >= 0; i--) {
+    randomizedArray.push(membersArray.splice(getRandomIntInclusive(0, i), 1)[0]);
+  };
+  return randomizedArray;
+};
+
+function generateAssignedTeams(members, method, quantity) {
+  const emptyArray = [];
+  const membersRandomizedArray = getRandomizedArray(members.split(',').map(member => member.trim()));
+
+  if (!method || !quantity) {
+    return emptyArray;
+  };
+
+  const numQuantity = parseInt(quantity, 10);
+  if (isNaN(numQuantity) || numQuantity <= 0) {
+    return emptyArray;
+  };
+
+  const teams = [];
+  if (method == "team_count") {
+    let numTeams = numQuantity;
+    let minMembersPerTeam = 1;
+    if (numTeams > membersRandomizedArray.length) {
+      numTeams = membersRandomizedArray.length;
+    } else {
+      minMembersPerTeam = Math.floor(membersRandomizedArray.length/numQuantity);
+      if (minMembersPerTeam <= 0)
+        minMembersPerTeam = 1;
+    };
+    while (membersRandomizedArray.length > 0) {
+      teams.unshift(membersRandomizedArray.splice(0,minMembersPerTeam).join(', '));
+      numTeams -= 1;
+      if (numTeams < 1)
+        numTeams = 1;
+      if (membersRandomizedArray.length%numTeams == 0)
+        minMembersPerTeam = membersRandomizedArray.length/numTeams;
+    };
+  } else if (method == "number_per_team") {
+    while (membersRandomizedArray.length > 0) {
+      teams.push(membersRandomizedArray.splice(0,numQuantity).join(', '));
+    };
+  };
+  return teams;
+};
+
 router.get("/:id", (req, res) => {
   knex("cohorts")
     .where("id", req.params.id)
     .first()
     .then((cohort) => {
-      res.render("cohorts/show", { cohort: cohort });
+      res.render("cohorts/show", {
+        cohort: cohort,
+        method: req.query.method,
+        quantity: req.query.quantity,
+        teams: generateAssignedTeams(cohort.members, req.query.method, req.query.quantity)
+      });
     });
 });
 
