588fe385b8d41cf208c26a3b18d394b5506c64b6
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-04-13 22:05:50 -0700

    Ending the Game

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,21 +1,50 @@
 $(document).ready(() => {
   const mysteryWord = "STRANGER";
+  const MAX_TRIES = 6;
+
   let fails = 0;
+  let matches = 0;
+
+  const applauseSound = new Audio("sounds/applause3.wav");
+  const bombSound = new Audio("sounds/bomb_x.wav");
 
   for (let i = 0; i < mysteryWord.length; i++) {
     $("table.mysteryWord tr").append($(`<td></td>`));
   }
 
+  function displayTheLetters(letter) {
+    let i = mysteryWord.indexOf(letter, 0);
+    while (i > -1) {
+      matches++;
+      $(`table.mysteryWord td:nth-child(${i + 1})`).text(letter);
+      i = mysteryWord.indexOf(letter, i + 1);
+    }
+  }
+
+  function displayNextImage() {
+    fails++;
+    $("img.hangmanImage").attr("src", `images/${fails}.jpg`);
+  }
+
+  function endTheGame(music, message) {
+    music.play();
+    setTimeout(() => {
+      $(alert(message));
+      location.reload();
+    }, 1000);
+  }
+
   function checkTheGuess(guess) {
     if (mysteryWord.includes(guess)) {
-      let i = mysteryWord.indexOf(guess, 0);
-      while (i > -1) {
-        $(`table.mysteryWord td:nth-child(${i + 1})`).text(guess);
-        i = mysteryWord.indexOf(guess, i + 1);
+      displayTheLetters(guess);
+      if (matches === mysteryWord.length) {
+        endTheGame(applauseSound, "Congratulations! You win!");
       }
     } else {
-      fails++;
-      $("img.hangmanImage").attr("src", `images/${fails}.jpg`);
+      displayNextImage();
+      if (fails === MAX_TRIES) {
+        endTheGame(bombSound, "Better luck next time...");
+      }
     }
   }
 
