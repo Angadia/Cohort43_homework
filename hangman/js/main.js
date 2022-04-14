8f8f56dfeda627811594374e7788f429397733b1
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-04-13 21:35:06 -0700

    Correct Guesses

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
+  const mysteryWord = "STRANGER";
+
+  for (let i = 0; i < mysteryWord.length; i++) {
+    $("table.mysteryWord tr").append($(`<td></td>`));
+  }
+
+  function checkTheGuess(guess) {
+    let i = mysteryWord.indexOf(guess, 0);
+    console.log(guess, i);
+    while (i > -1) {
+      $(`table.mysteryWord td:nth-child(${i+1})`).text(guess);
+      i = mysteryWord.indexOf(guess, i + 1);
+    }
+  }
+
   $("table.alphabets td").on("click", (e) => {
     const currentTarget = $(e.currentTarget);
     currentTarget.addClass("selected");
+    checkTheGuess(currentTarget.text());
   });
 });
